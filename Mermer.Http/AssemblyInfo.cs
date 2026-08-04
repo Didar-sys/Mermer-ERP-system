11cc1b4f0d2e8e864d7369552f987aba37f7613e
Maksim <maksimklimik6@gmail.com>
2026-08-04 14:46:57 +0300

    Исправил небольшую ошибку в Mermer.Http

--- Mermer.Http/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyCompany("Mermer.Http")]
-[assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
-[assembly: AssemblyProduct("Mermer.Http")]
-[assembly: AssemblyTitle("Mermer.Http")]
-[assembly: AssemblyVersion("1.0.0.0")]
